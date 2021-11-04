581e9dce0cc5cb7530f1c7878897ac6e10aac26b
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2021-11-04 20:00:45 -0300

    Update SistemaRitoGames.java

--- Taller1_Editable/logica/SistemaRitoGames.java
+++ Taller1_Editable/logica/SistemaRitoGames.java
@@ -1,5 +1,18 @@
 package Taller1.logica;
 
-public class SistemaRitoGames {
+public interface SistemaRitoGames {
+    
+    boolean agregarUsuario(String nombreCuenta, String password, String nick, int nivel, int rp, int totalPersonajes, String nombrePersonajes, int totalSkins, String nombreSkins, String region);
+
+    boolean agregarPersonaje(String nombrePersonaje, String rol, int totalSkins, String nombreSkins, int estadisticas);
+
+    void setRecaudacionPersonaje(String nombrePersonaje, int estadisticas);
+
+    boolean agregarSkin(String nombreSkin, String nombrePersonaje, String calidad);
+
+    void asociarPersonajeUsuario(String nombrePersonaje, String nombreCuenta);
+
+    void asociarSkinPersonaje(String nombreSkin, String nombrePersonaje);
+
     
 }
