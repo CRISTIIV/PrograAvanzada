c6090a2ff32698d3a3960c3cf724ffa7a9ba9f92
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2021-11-06 18:20:38 -0300

    Update ListaPersonaje.java

--- Taller1_Git/logica/ListaPersonaje.java
+++ Taller1_Git/logica/ListaPersonaje.java
@@ -1,5 +1,81 @@
 package Taller1_Git.logica;
+import Taller1_Git.dominio.Personaje;
 
 public class ListaPersonaje {
+    private int cantidad;
+    private int maximo;
+    private Personaje[] listaPersonajes;
+
+    public ListaPersonaje(int maximo){
+        this.cantidad = 0;
+        this.maximo = maximo;
+        listaPersonajes = new Personaje[maximo];
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getMaximo() {
+        return maximo;
+    }
+
+    public void setMaximo(int maximo) {
+        this.maximo = maximo;
+    }
+
+    public Personaje[] getListaPersonajes() {
+        return listaPersonajes;
+    }
+
+    public void setListaPersonajes(Personaje[] listaPersonajes) {
+        this.listaPersonajes = listaPersonajes;
+    }
     
+    public boolean insertar(Personaje personaje){
+        if (cantidad < maximo){
+            listaPersonajes[cantidad]=personaje;
+            cantidad++;
+            return true;
+        }
+        return false;
+    }
+    
+    public Personaje buscarPorNombrePersonaje(String nombrePersonaje){
+        for (int i = 0; i < cantidad; i++){
+            Personaje personaje = listaPersonajes[i];
+            if (personaje.getNombrePersonaje().equals(nombrePersonaje)){
+                return personaje;
+            }
+        }
+        return null;
+    }
+
+    public Personaje buscarPorI(int i){
+        Personaje personaje = listaPersonajes[i];
+        if (personaje!=null){
+            return personaje;
+        }
+        return null;
+    }
+
+    public boolean agregarNewPersonaje(String nombreNewPersonaje){
+        if (cantidad < maximo){
+            listaPersonajes[cantidad]=nombreNewPersonaje;
+            cantidad++;
+            return true;
+        }
+        if (cantidad = maximo){
+            maximo+=1;
+            listaPersonajes[cantidad]=nombreNewPersonaje;
+            cantidad++;
+            return true;
+        }
+        return false;
+    }
+
 }
