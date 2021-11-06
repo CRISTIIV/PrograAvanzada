d6b1020813c17dbf6b303d798603f58dcd16d08d
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2021-11-06 18:20:31 -0300

    Update ListaUsuario.java

--- Taller1_Git/logica/ListaUsuario.java
+++ Taller1_Git/logica/ListaUsuario.java
@@ -1,5 +1,66 @@
 package Taller1_Git.logica;
+import Taller1_Git.dominio.Usuario;
 
 public class ListaUsuario {
-    
+    private int cantidad;
+    private int maximo;
+    private Usuario[] listaUsuarios;
+
+    public ListaUsuario(int maximo){
+        this.cantidad = 0;
+        this.maximo = maximo;
+        listaUsuarios = new Usuario[maximo];
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getMaximo() {
+        return maximo;
+    }
+
+    public void setMaximo(int maximo) {
+        this.maximo = maximo;
+    }
+
+    public Usuario[] getListaUsuarios() {
+        return listaUsuarios;
+    }
+
+    public void setListaUsuarios(Usuario[] listaUsuarios) {
+        this.listaUsuarios = listaUsuarios;
+    }
+
+    public boolean insertar(Usuario usuario){
+        if (cantidad < maximo){
+            listaUsuarios[cantidad]=usuario;
+            cantidad++;
+            return true;
+        }
+        return false;
+    }
+
+    public Usuario buscarPorNombreCuenta(String nombreCuenta){
+        for (int i = 0; i < cantidad; i++){
+            Usuario usuario = listaUsuarios[i];
+            if (usuario.getNombreCuenta().equals(nombreCuenta)){
+                return usuario;
+            }
+        }
+        return null;
+    }
+
+    public Usuario buscarPorI(int i){
+        Usuario usuario = listaUsuarios[i];
+        if (usuario!=null){
+            return usuario;
+        }
+        return null;
+    }
+
 }
