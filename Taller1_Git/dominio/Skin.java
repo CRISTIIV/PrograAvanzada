226e42869337603f5d84c8e1c3d18916167d25d3
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2021-11-06 18:20:39 -0300

    Update Skin.java

--- Taller1_Git/dominio/Skin.java
+++ Taller1_Git/dominio/Skin.java
@@ -1,5 +1,48 @@
 package Taller1_Git.dominio;
+import Taller1_Git.dominio.Personaje;
 
 public class Skin {
+    private String nombreSkin;
+    private String calidad;
+    private int precioSkin;
+    private Personaje personaje;
+    
+    public Skin(String nombreSkin, String calidad, int precioSkin){
+        this.nombreSkin = nombreSkin;
+        this.calidad = calidad;
+        this.precioSkin = precioSkin;
+    }
+
+    public String getNombreSkin() {
+        return nombreSkin;
+    }
+
+    public void setNombreSkin(String nombreSkin) {
+        this.nombreSkin = nombreSkin;
+    }
+
+    public String getCalidad() {
+        return calidad;
+    }
+
+    public void setCalidad(String calidad) {
+        this.calidad = calidad;
+    }
+
+    public int getPrecioSkin() {
+        return precioSkin;
+    }
+
+    public void setPrecioSkin(int precioSkin) {
+        this.precioSkin = precioSkin;
+    }
+
+    public Personaje getPersonaje() {
+        return personaje;
+    }
+
+    public void setPersonaje(Personaje personaje) {
+        this.personaje = personaje;
+    }
     
 }
