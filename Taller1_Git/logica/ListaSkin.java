ba207db2071c76674e3d1935f908f311c442dfe1
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2021-11-06 18:20:34 -0300

    Update ListaSkin.java

--- Taller1_Git/logica/ListaSkin.java
+++ Taller1_Git/logica/ListaSkin.java
@@ -1,5 +1,39 @@
 package Taller1_Git.logica;
+import Taller1_Git.dominio.Skin;
 
 public class ListaSkin {
-    
+    private int cantidad;
+    private int maximo;
+    private Skin[] listaSkins;
+
+    public ListaSkin(int maximo){
+        this.cantidad = 0;
+        this.maximo = maximo;
+        listaSkins = new Skin[maximo];
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getMaximo() {
+        return maximo;
+    }
+
+    public void setMaximo(int maximo) {
+        this.maximo = maximo;
+    }
+
+    public Skin[] getListaSkins() {
+        return listaSkins;
+    }
+
+    public void setListaSkins(Skin[] listaSkins) {
+        this.listaSkins = listaSkins;
+    }
+
 }
