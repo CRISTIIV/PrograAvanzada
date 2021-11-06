f893654f47b9f5bfeff914934ec0c7e907a690ac
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2021-11-06 18:20:43 -0300

    Update Personaje.java

--- Taller1_Git/dominio/Personaje.java
+++ Taller1_Git/dominio/Personaje.java
@@ -2,5 +2,47 @@ package Taller1_Git.dominio;
 import Taller1_Git.logica.ListaSkin;
 
 public class Personaje {
-    
+    private String nombrePersonaje;
+    private String rol;
+    private int precioPersonaje;
+    private int recaudacionPersonaje;
+    private ListaSkin listaSkins;
+
+    public Personaje(String nombrePersonaje, String rol, int precioPersonaje, int recaudacionPersonaje){
+        this.nombrePersonaje = nombrePersonaje;
+        this.rol = rol;
+        this.precioPersonaje = precioPersonaje;
+        this.recaudacionPersonaje = recaudacionPersonaje;
+        listaSkins = new ListaSkin(500);
+    }
+    public String getNombrePersonaje() {
+        return nombrePersonaje;
+    }
+    public void setNombrePersonaje(String nombrePersonaje) {
+        this.nombrePersonaje = nombrePersonaje;
+    }
+    public String getRol() {
+        return rol;
+    }
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+    public int getPrecioPersonaje() {
+        return precioPersonaje;
+    }
+    public void setPrecioPersonaje(int precioPersonaje) {
+        this.precioPersonaje = precioPersonaje;
+    }
+    public int getRecaudacionPersonaje() {
+        return recaudacionPersonaje;
+    }
+    public void setRecaudacionPersonaje(int recaudacionPersonaje) {
+        this.recaudacionPersonaje = recaudacionPersonaje;
+    }
+    public ListaSkin getListaSkins() {
+        return listaSkins;
+    }
+    public void setListaSkins(ListaSkin listaSkins) {
+        this.listaSkins = listaSkins;
+    }
 }
