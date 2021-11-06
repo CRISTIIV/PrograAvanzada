54886a1b7e99e63a2ecc95151f3b36f5bb6caa22
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2021-11-05 23:26:39 -0300

    Update Main.java

--- Taller1_Git/logica/Main.java
+++ Taller1_Git/logica/Main.java
@@ -1,5 +1,16 @@
 package Taller1_Git.logica;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
 
 public class Main {
-    
+    public static void main(String[] args){
+        SistemaRitoGames sistema = new SistemaRitoGamesImpl();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bienvenido al sistema de compra de Personajes y Skin para el videojuego <Juego Generico>");
+        lecturaCuentas(sistema);
+        lecturaPersonajes(sistema);
+        lecturaEstadisticas(sistema);
+    }
 }
