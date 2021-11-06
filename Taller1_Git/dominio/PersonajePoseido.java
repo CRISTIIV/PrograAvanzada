bf6f218f788e991abdb54e50a910fb9a89b5876c
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2021-11-06 18:20:41 -0300

    Update PersonajePoseido.java

--- Taller1_Git/dominio/PersonajePoseido.java
+++ Taller1_Git/dominio/PersonajePoseido.java
@@ -1,5 +1,43 @@
 package Taller1_Git.dominio;
+import Taller1_Git.logica.ListaSkin;
+import Taller1_Git.dominio.Usuario;
+import Taller1_Git.dominio.Personaje;
 
 public class PersonajePoseido {
+    
+    private Usuario propietario;
+    private Personaje personaje;
+    private ListaSkin listaSkinsPoseidos;
+
+    public PersonajePoseido(Usuario propietario, Personaje personaje){//SEGÚN YO NO SE COLOCA NADA EN EL PARENTESIS ESTA VEZ Y ABAJO SOLO LA LISTA, 
+                                                                      //PERO QUEDARÍA VACÍO ACÁ, ENTONCES NO SÉ:(
+        this.propietario = propietario;
+        this.personaje = personaje;
+        listaSkinsPoseidos = new ListaSkin(500);
+    }
+
+    public Usuario getPropietario() {
+        return propietario;
+    }
+
+    public void setPropietario(Usuario propietario) {
+        this.propietario = propietario;
+    }
+
+    public Personaje getPersonaje() {
+        return personaje;
+    }
+
+    public void setPersonaje(Personaje personaje) {
+        this.personaje = personaje;
+    }
+
+    public ListaSkin getListaSkinsPoseidos() {
+        return listaSkinsPoseidos;
+    }
+
+    public void setListaSkinsPoseidos(ListaSkin listaSkinsPoseidos) {
+        this.listaSkinsPoseidos = listaSkinsPoseidos;
+    }
     
 }
