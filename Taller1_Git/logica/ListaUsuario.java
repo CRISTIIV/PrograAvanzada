d7524a45273f45e83d435876d8b54916d07848a9
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2021-11-08 16:37:04 -0300

    Update ListaUsuario.java

--- Taller1_Git/logica/ListaUsuario.java
+++ Taller1_Git/logica/ListaUsuario.java
@@ -1,10 +1,13 @@
 package Taller1_Git.logica;
+import Taller1_Git.dominio.PersonajePoseido;
+import Taller1_Git.dominio.Skin;
 import Taller1_Git.dominio.Usuario;
 
 public class ListaUsuario {
     private int cantidad;
     private int maximo;
     private Usuario[] listaUsuarios;
+    private Skin skin;
 
     public ListaUsuario(int maximo){
         this.cantidad = 0;
@@ -34,6 +37,14 @@ public class ListaUsuario {
 
     public void setListaUsuarios(Usuario[] listaUsuarios) {
         this.listaUsuarios = listaUsuarios;
+    }
+
+    public Skin getSkin() {
+        return skin;
+    }
+
+    public void setSkin(Skin skin) {
+        this.skin = skin;
     }
 
     public boolean insertar(Usuario usuario){
@@ -72,7 +83,7 @@ public class ListaUsuario {
         return salida;
     }
     //ARREGLARLO
-    public String formatoRecRegion(String nombreCuenta){
+    public String formatoRecRegion(){
         int las = 0;
         int lan = 0;
         int euw = 0;
@@ -80,26 +91,127 @@ public class ListaUsuario {
         int na = 0;
         int ru = 0;
         String salida = "";
-        Usuario usuario = listaUsuarios[i];
-        if (usuario.getRegion().equals("LAS")){
-            las += this.formatoEstadisticas();
-        }
-        if (usuario.getRegion().equals("LAN")){
-            lan += this.formatoEstadisticas();
-        }
-        if (usuario.getRegion().equals("EUW")){
-            euw += this.formatoEstadisticas();
-        }
-        if (usuario.getRegion().equals("KR")){
-            kr += this.formatoEstadisticas();
-        }
-        if (usuario.getRegion().equals("NA")){
-            na += this.formatoEstadisticas();
-        }
-        if (usuario.getRegion().equals("RU")){
-            ru += this.formatoEstadisticas();
-        }
-        salida += "Las recaudaciones por rol son: \n SUPORT: $"+sup+" clp \n ATACK DAMAGE CARRY: $"+adc+" clp \n TOP LANER: $"+top+" clp \n MIDDLE LANER: $"+mid+" clp \n JUNGLER: $"+jg+" clp.";
+        for (int i = 0; i < cantidad; i++){
+            Usuario usuario = listaUsuarios[i];
+            for (int j = 0; j < usuario.getListaPersonajesPoseidos().getCantidad(); j++){
+                Usuario usuario2 = usuario.getListaPersonajesPoseidos().buscarPorI(j);
+                if (usuario2.getRegion().equals("LAS")){
+                    las += 975;
+                }
+                if (usuario2.getRegion().equals("LAN")){
+                    lan += 975;
+                }
+                if (usuario2.getRegion().equals("EUW")){
+                    euw += 975;
+                }
+                if (usuario2.getRegion().equals("KR")){
+                    kr += 975;
+                }
+                if (usuario2.getRegion().equals("NA")){
+                    na += 975;
+                }
+                if (usuario2.getRegion().equals("RU")){
+                    ru += 975;
+                }
+            }
+            for (int k = 0; k < usuario.getListaSkinsPoseidas().getCantidad(); k++){
+                Usuario usuario3 = usuario.getListaSkinsPoseidas().buscarPorI(k);
+                Skin skin2 = usuario3.getListaSkinsPoseidas().buscarPorI(k);
+                if (skin2.getCalidad().equals("M")){
+                    if (usuario3.getRegion().equals("LAS")){
+                        las += 3250;
+                    }
+                    if (usuario3.getRegion().equals("LAN")){
+                        lan += 3250;
+                    }
+                    if (usuario3.getRegion().equals("EUW")){
+                        euw += 3250;
+                    }
+                    if (usuario3.getRegion().equals("KR")){
+                        kr += 3250;
+                    }
+                    if (usuario3.getRegion().equals("NA")){
+                        na += 3250;
+                    }
+                }
+                if (skin2.getCalidad().equals("D")){
+                    if (usuario3.getRegion().equals("LAS")){
+                        las += 2750;
+                    }
+                    if (usuario3.getRegion().equals("LAN")){
+                        lan += 2750;
+                    }
+                    if (usuario3.getRegion().equals("EUW")){
+                        euw += 2750;
+                    }
+                    if (usuario3.getRegion().equals("KR")){
+                        kr += 2750;
+                    }
+                    if (usuario3.getRegion().equals("NA")){
+                        na += 2750;
+                    }
+                }
+                if (skin2.getCalidad().equals("L")){
+                    if (usuario3.getRegion().equals("LAS")){
+                        las += 1820;
+                    }
+                    if (usuario3.getRegion().equals("LAN")){
+                        lan += 1820;
+                    }
+                    if (usuario3.getRegion().equals("EUW")){
+                        euw += 1820;
+                    }
+                    if (usuario3.getRegion().equals("KR")){
+                        kr += 1820;
+                    }
+                    if (usuario3.getRegion().equals("NA")){
+                        na += 1820;
+                    }
+                }
+                if (skin2.getCalidad().equals("E")){
+                    if (usuario3.getRegion().equals("LAS")){
+                        las += 1350;
+                    }
+                    if (usuario3.getRegion().equals("LAN")){
+                        lan += 1350;
+                    }
+                    if (usuario3.getRegion().equals("EUW")){
+                        euw += 1350;
+                    }
+                    if (usuario3.getRegion().equals("KR")){
+                        kr += 1350;
+                    }
+                    if (usuario3.getRegion().equals("NA")){
+                        na += 1350;
+                    }
+                }
+                if (skin2.getCalidad().equals("N")){
+                    if (usuario3.getRegion().equals("LAS")){
+                        las += 975;
+                    }
+                    if (usuario3.getRegion().equals("LAN")){
+                        lan += 975;
+                    }
+                    if (usuario3.getRegion().equals("EUW")){
+                        euw += 975;
+                    }
+                    if (usuario3.getRegion().equals("KR")){
+                        kr += 975;
+                    }
+                    if (usuario3.getRegion().equals("NA")){
+                        na += 975;
+                    }
+                }
+            }
+        double lasClp = las * (6.15);
+        double lanClp = lan * (6.15);
+        double euwClp = euw * (6.15);
+        double krClp = kr * (6.15);
+        double naClp = na * (6.15);
+        double ruClp = ru * (6.15);
+       
+        salida += "Las recaudaciones por region son: \n LAS: $"+lasClp+" clp \n LAN: $"+lanClp+" clp \n EUW: $"+euwClp+" clp \n KR: $"+krClp+" clp \n NA: $"+naClp+" clp \n RU: $"+ruClp+" clp.";
+        return salida;
     }
 
 }
