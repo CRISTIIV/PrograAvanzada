0b0fc2b58583c3594a6f9cb6fe82efd95f19f888
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2021-11-09 00:33:32 -0300

    Update Main.java

--- Taller1_Git/logica/Main.java
+++ Taller1_Git/logica/Main.java
@@ -50,19 +50,23 @@ public class Main {
                 int nivel = Integer.parseInt(parte[3]);
                 int rp = Integer.parseInt(parte[4]);
                 int totalPersonajes = Integer.parseInt(parte[5]);
+                int totalSkins2 = 0;
+                String nombresSkins = "";
+                String nombresPersonajes = "";
                 for (int i = 0; i < totalPersonajes; i++){
                     String nombrePersonajes = parte[6+i];
-                    nombrePersonajes += ",";
+                    nombresPersonajes += nombrePersonajes+",";
                     int totalSkins = Integer.parseInt(parte[6+totalPersonajes]);
+                    totalSkins2 += totalSkins;
                     for (int j = 0; j < totalSkins; j++){
                         String nombreSkins = parte[7+totalPersonajes];
-                        nombreSkins += ",";
+                        nombresSkins += nombreSkins+",";
                     }
                 }
-                String region = parte[8+totalPersonajes+totalSkins];
+				String region = parte[8+totalPersonajes+totalSkins2];
     
                 try {
-                    sistema.agregarUsuario(nombreCuenta, password, nick, nivel, rp, totalPersonajes, nombrePersonajes, totalSkins, nombreSkins, region);
+                    sistema.agregarUsuario(nombreCuenta, password, nick, nivel, rp, totalPersonajes, nombresPersonajes, totalSkins2, nombresSkins, region);
                 } catch (Exception e) {
                     System.out.println("No se pudo agregar la cuenta en la linea" + contador + "ERROR: "+ e.getMessage());
                 }
@@ -74,8 +78,68 @@ public class Main {
         }
     }
 
+    public static void lecturaPersonajes(SistemaRitoGames sistema) {
+        int contador = 1;
+        try {
+            Scanner archivo = new Scanner(new File("Personajes.txt"));
+            while (archivo.hasNextLine()){
+                
+                String linea = archivo.nextLine();
+                String[] parte = linea.split(",");
+    
+                String nombrePersonaje = parte[0];
+                String rol = parte[1];
+                int totalSkins = Integer.parseInt(parte[2]);
+                String nombreSkins = "";
+                String calidad = "";
+                for (int i = 0; i < totalSkins; i++) {
+                	String variableNombre = parte[3+i];
+                	nombreSkins += variableNombre+",";
+                	String variableCalidad = parte[4+i];
+                	calidad += variableCalidad+",";
+                }
+                int recaudacionesPersonaje = Integer.parseInt(parte[5+totalSkins]);
+    
+                try {
+                    sistema.agregarPersonaje(nombrePersonaje,rol,totalSkins,nombreSkins,recaudacionesPersonaje);
+                    sistema.agregarSkin(nombreSkins, calidad, nombrePersonaje);
+                } catch (Exception e) {
+                    System.out.println("No se pudo agregar el personaje y las skin en la linea" + contador + "ERROR: "+ e.getMessage());
+                }
+                contador++;
+    
+            }
+        } catch (Exception e) {
+            System.out.println("No se pudo leer el archivo Personajes.txt");
+        }
+    }
+
+    public static void lecturaEstadisticas(SistemaRitoGames sistema) {
+        int contador = 1;
+        try {
+            Scanner archivo = new Scanner(new File("Estadisticas.txt"));
+            while (archivo.hasNextLine()){
+                
+                String linea = archivo.nextLine();
+                String[] parte = linea.split(",");
+    
+                String nombrePersonaje = parte[0];
+                int recaudacion = Integer.parseInt(parte[1]);
+                try {
+                    sistema.agregarEstadistica(nombrePersonaje,recaudacion);
+                } catch (Exception e) {
+                    System.out.println("No se pudo agregar la recaudacion en la linea" + contador + "ERROR: "+ e.getMessage());
+                }
+                contador++;
+    
+            }
+        } catch (Exception e) {
+            System.out.println("No se pudo leer el archivo Estadisticas.txt");
+        }
+    }
+    
     public static void mainMenu(SistemaRitoGames sistema, String nombreCuenta, String password){
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         if(nombreCuenta.equals("ADMIN")) {
             if (password.equals("ADMIN")){
                 menuAdmin(sistema);
@@ -85,21 +149,21 @@ public class Main {
         }else{
             String validacion = sistema.iniciarSesion(nombreCuenta, password);
             switch (validacion) {
-                case "La cuenta ingresada no existe, ¿desea registrase?"://ACA CAMBIÉ POR EL TEXTO DEL return EN EL INICIAR SESION DEL TALLER NUEVO
-                    System.out.println("La cuenta ingresada no existe, ¿desea registrase?(si-no)");
-                    String registro = sc.next().toLowerCase();
+                case "La cuenta ingresada no existe, Â¿desea registrase?"://ACA CAMBIÃ‰ POR EL TEXTO DEL return EN EL INICIAR SESION DEL TALLER NUEVO
+                    System.out.println("La cuenta ingresada no existe, Â¿desea registrase?(si-no)");
+                    String registro = scanner.next().toLowerCase();
                     if(registro.equals("si")){
                         System.out.println("Ingresa el nombre de tu nueva cuenta: ");
-                        String nombreCuentaNueva = sc.next();
+                        String nombreCuentaNueva = scanner.next();
                         
                         System.out.println("Ingrese su nueva contrasena: ");
-                        String passwordNueva = sc.next();
+                        String passwordNueva = scanner.next();
                         
                         System.out.println("Ingrese su nuevo nick: ");
-                        String nickNuevo = sc.next();
+                        String nickNuevo = scanner.next();
 
                         System.out.println("Ingrese su region: ");
-                        String regionNueva = sc.next();
+                        String regionNueva = scanner.next();
                         regionNueva = hacerMayuscula(regionNueva);
 
                         int nivelInicial = 0;
@@ -119,7 +183,7 @@ public class Main {
                     break;
 
                 case "Bienvenido!":
-                    menuCliente(rut,sistema);
+                    menuCliente(nombreCuenta,sistema);
                     break;
                     
                 case "Ingresaste mal la contrasena":
@@ -133,7 +197,7 @@ public class Main {
     }
     
     public static void menuAdmin(SistemaRitoGames sistema){
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Bienvenido ADMINISTRADOR, Que desea realizar? ");
         System.out.println("\n1) Desplegar recaudacion de ventas por rol");
         System.out.println("2) Desplegar recaudacion total de ventas por region");
@@ -145,7 +209,7 @@ public class Main {
         System.out.println("8) Desplegar todas las cuentas de mayor a menor");
         System.out.println("\nDigite 'salir' para salir");
         while(true){
-            String opcion = sc.next();
+            String opcion = scanner.next();
             Boolean salir = false;
             switch (opcion) {
                 case "1":
@@ -171,13 +235,15 @@ public class Main {
                     System.out.println("\nIngrese el rol del personaje: ");
                     String nuevoRol = scanner.next();
                     System.out.println("\nIngrese la cantidad de skins que tiene el personaje: ");
-                    int nuevoTotalSkins = scanner.next();
+                    int nuevoTotalSkins = Integer.parseInt(scanner.next());
+                    String nuevoNombresSkins = "";
                     for (int i = 0; i < nuevoTotalSkins; i++){
-                        System.out.println("\nIngrese el nombre de la skin N°"+(i+1)+": ");
+                        System.out.println("\nIngrese el nombre de la skin NÂ°"+(i+1)+": ");
                         String nuevoNombreSkins = scanner.next()+",";
+                        nuevoNombresSkins += nuevoNombreSkins;
                     }
                     int nuevaRecaudacionPersonaje = 0;
-                    sistema.agregarNuevoPersonaje(nuevoNombre, nuevoRol, nuevoTotalSkins, nuevoNombreSkins, nuevaRecaudacionPersonaje);
+                    sistema.agregarNuevoPersonaje(nuevoNombre, nuevoRol, nuevoTotalSkins, nuevoNombresSkins, nuevaRecaudacionPersonaje);
                     break;
 
                 case "6":
@@ -221,7 +287,7 @@ public class Main {
     }
 
     public static void menuCliente(String nombreCuenta,SistemaRitoGames sistema) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Bienvenido, Que desea realizar?");
         System.out.println("\n1) Desplegar skins disponibles para comprar");
         System.out.println("2) Comprar Skin");
@@ -232,7 +298,7 @@ public class Main {
         System.out.println("7) Cambiar contrasena");
         System.out.println("\nDigite 'salir' para salir");
         while(true){
-            String opcion = sc.next();
+            String opcion = scanner.next();
             Boolean salir = false;
             switch (opcion) {
                 case "1":
@@ -259,7 +325,7 @@ public class Main {
 
                 case "5":
                     System.out.println("Ingrese la cantidad de rp a agregar: ");
-                    int rp = sc.nextInt();
+                    int rp = scanner.nextInt();
                     try {
                         sistema.recargarRP(nombreCuenta,rp);
                         System.out.println("Se agregaron: " +rp+ " a la cuenta.");
@@ -274,11 +340,11 @@ public class Main {
                     
                 case "7":
                     System.out.println("Ingrese su contrasena actual");
-                    String oldPassword = sc.next();
+                    String oldPassword = scanner.next();
                     System.out.println("Ingrese la nueva contrasena");
-                    String newPassword = sc.next();
+                    String newPassword = scanner.next();
                     System.out.println("Confirme la nueva contrasena");
-                    String newPasswordConf = sc.next();
+                    String newPasswordConf = scanner.next();
                     if(sistema.iniciarSesion(nombreCuenta,oldPassword).equals("Bienvenido!") && newPassword.equals(newPasswordConf)) {
                         try {
                             sistema.cambiarPassword(nombreCuenta,oldPassword,newPassword);
@@ -360,3 +426,4 @@ public class Main {
 
 
 }
+
